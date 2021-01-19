cdf9037171fc450f64c0da59fc2c37307261689b
XxXPahaPro97XxX <zergood666@yandex.ru>
2021-01-20 06:06:49 +0700

    добавление методов

--- 1.py
+++ 1.py
@@ -5,4 +5,18 @@ class work:
         self.rb=works
         self.tv=goods
 
-       
+    def __return__(self):
+        return "\n Количество работ в час = {} \n Количество окон = {} \n Время обслуживания одного клиента = {} \n  Продаваемых товаров в час = {}".format (self.rb,self.ok,self.vr,self.tv)
+    def display_info(self):
+        print("Среднее количество выполняемых работ в час:",self.rb)
+        print("Количество окон для обслуживания:",self.ok)
+        print("Среднее время обслуживания одного клиента:",self.vr)
+        print("Среднее количество продаваемых товаров в час:",self.tv)
+    def virab(self):
+        self.virab=self.rb // self.vr
+        return "\n Выработка предприятия за %s часа(ов) составляет %s" % (self.vr,self.virab)
+    def bolsheokon(self):
+        self.bolsheokon=self.ok + 1
+        self.rb=self.rb * 1.33 
+        self.virab=self.rb // self.vr   
+        return "\n Выработка предприятия за %s часа(ов) составляет %s" % (self.vr,self.virab)
