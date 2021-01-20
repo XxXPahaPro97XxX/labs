7476de3dcaec62671de3355a666f4fe838d4cf25
XxXPahaPro97XxX <zergood666@yandex.ru>
2021-01-20 07:01:27 +0700

    Создание таблицы и внесение данных

--- 3.py
+++ 3.py
@@ -3,3 +3,7 @@ import sqlite3
 
 conn=sqlite3.connect("basadannih.db")
 cursor=conn.cursor()
+
+cursor.execute("""CREATE TABLE Predpriyatie (name text, okna integer, vremya integer, raboti integer, tovari integer) """)
+Predpriyatie = [('Pered Avtomatization', '8','5','60','81'),('Posle Avtmzn','12','8','97','169')]
+cursor.executemany("INSERT INTO Predpriyatie VALUES (?,?,?,?,?)", Predpriyatie)
