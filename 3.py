b708c2e6d67e89f74a8ad0612ec29e66bdd93619
XxXPahaPro97XxX <zergood666@yandex.ru>
2021-01-20 07:07:51 +0700

    запрос для выборки данных из таблицы

--- 3.py
+++ 3.py
@@ -7,3 +7,11 @@ cursor=conn.cursor()
 cursor.execute("""CREATE TABLE Predpriyatie (name text, okna integer, vremya integer, raboti integer, tovari integer) """)
 Predpriyatie = [('Pered Avtomatization', '8','5','60','81'),('Posle Avtmzn','12','8','97','169')]
 cursor.executemany("INSERT INTO Predpriyatie VALUES (?,?,?,?,?)", Predpriyatie)
+
+sqlupdate="""
+UPDATE Predpriyatie
+SET okna = '10', vremya = '6', raboti = '89', tovari = '149' 
+WHERE name = 'Posle Avtmzn'
+
+cursor.execute(sqlupdate)
+conn.commit()
